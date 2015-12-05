9c018913fc1cfeb529f7b430b2570f805bb33df1
Vitalii Petrychuk <vitaliy@petrychuk.com>
2015-12-05 09:44:48 +0100

    Day 5/1

---
+++ day-5/1.js
@@ -0,0 +1,37 @@
+var input = String(require('fs').readFileSync(__dirname + '/input.txt'));
+
+var vowels = 'aeiou'.split('');
+var forbidden = ['ab', 'cd', 'pq', 'xy'];
+var strings = input.split('\n');
+
+function isNice(string) {
+	var forbiddenString = forbidden.find(phrase => string.indexOf(phrase) !== -1);
+	if (forbiddenString) {
+		return false;
+	}
+
+	var foundVowels = 0;
+	var previousChar = null;
+	var hasDoubleChar = false;
+	for (var i = 0; i < string.length; i++) {
+		var char = string[i];
+		if (previousChar === char) {
+			hasDoubleChar = true;
+		}
+		if (vowels.indexOf(char) !== -1) {
+			foundVowels += 1;
+		}
+		previousChar = char;
+	}
+
+	return hasDoubleChar && foundVowels >= 3;
+}
+
+var amount = 0;
+for (var i = 0; i < strings.length; i++) {
+	if (isNice(strings[i])) {
+		amount++;
+	}
+}
+
+console.log(amount);
