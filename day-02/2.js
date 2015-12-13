18c8492c7cf843cb91d5f387eab4634ca288cb9f
Vitalii Petrychuk <vitaliy@petrychuk.com>
2015-12-13 12:18:35 +0100

    Use sort util for day 2

--- day-02/2.js
+++ day-02/2.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var sort = require('../utils/sort');
+
 module.exports = (input) => {
 
   var calculate = (dimensions) => {
-    var dimensionsArr = dimensions.split('x').map(Number).sort((a, b) => a > b);
+    var dimensionsArr = sort.asc(dimensions.split('x').map(Number));
     var side1 = dimensionsArr[0];
     var side2 = dimensionsArr[1];
     var side3 = dimensionsArr[2];
