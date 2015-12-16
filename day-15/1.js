e9a5e5301d73d4ba2324e349b24babf4b9042bca
Vitalii Petrychuk <vitaliy@petrychuk.com>
2015-12-16 19:06:50 +0100

    Day 15

---
+++ day-15/1.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var permute = require('../utils/permute');
+var max = require('../utils/max');
+
+module.exports = (input) => {
+
+  var RE = /([a-zA-Z]+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+)/;
+
+  var generateOptions = (ingredients) => permute.range(0, 100, ingredients.length);
+  var parseIngredients = (data) => {
+    return data.map(line => line.match(RE).slice(2).map(Number));
+  };
+
+  var multiplyProperties = (result) => {
+    return result.reduce((prev, current) => prev * (current < 0 ? 0 : current), 1);
+  };
+
+  var calculateScores = (options, ingredients) => {
+    return options.map(percentages => {
+      var propsLength = ingredients[0].length;
+      var result = new Array(propsLength);
+
+      for (var propIndex = 0; propIndex < propsLength; propIndex++) {
+        result[propIndex] = 0;
+        for (var ingredientIndex = 0; ingredientIndex < ingredients.length; ingredientIndex++) {
+          result[propIndex] += ingredients[ingredientIndex][propIndex] * percentages[ingredientIndex];
+        }
+      }
+
+      return multiplyProperties(result);
+    });
+  };
+
+  var ingredients = parseIngredients(input.trim().split('\n'));
+  var options = generateOptions(ingredients);
+  var scores = calculateScores(options, ingredients);
+
+  return max(scores);
+
+};
--- utils/permute.js
+++ utils/permute.js
@@ -1,19 +1,36 @@
 'use strict';
 
-var permute = (chars, memo, result) => {
+var permuteList = (list, memo, result) => {
   memo || (memo = []);
   result || (result = []);
 
-  for (var i = 0; i < chars.length; i++) {
-    var char = chars.splice(i, 1);
-    if (chars.length === 0) {
+  for (var i = 0; i < list.length; i++) {
+    var char = list.splice(i, 1);
+    if (list.length === 0) {
       result.push(memo.concat(char));
     }
-    permute(chars.slice(), memo.concat(char), result);
-    chars.splice(i, 0, char[0]);
+    permuteList(list.slice(), memo.concat(char), result);
+    list.splice(i, 0, char[0]);
   }
 
   return result;
 };
 
-module.exports = permute;
+var permuteRange = (min, max, size) => {
+  var perms = [];
+  if (size > 1) {
+    for (var i = min; i <= max; i++) {
+      var values = permuteRange(min, max - i, size - 1);
+      for (var j = min; j < values.length; j++) {
+        perms.push([i].concat(values[j]));
+      }
+    }
+    return perms;
+  }
+  else {
+    return [max];
+  }
+};
+
+module.exports.list = permuteList;
+module.exports.range = permuteRange;
