ad931a560b43d49c36a9e76964ae158e2ac4e5cd
Vitalii Petrychuk <vitaliy@petrychuk.com>
2024-12-07 15:09:38 +0100

    Day 7

---
+++ 2024/day-07/2.js
@@ -0,0 +1,22 @@
+module.exports = (input) => {
+  const map = input.split("\n").map((l) => {
+    const split = l.split(": ");
+    return [Number(split[0]), ...split[1].split(" ").map(Number)];
+  });
+
+  const evaluate = (target, acc, ...numbers) => {
+    if (numbers.length === 0) {
+      return acc === target;
+    }
+    const [nextNumber, ...restNumbers] = numbers;
+    return (
+      evaluate(target, acc + nextNumber, ...restNumbers) ||
+      evaluate(target, acc * nextNumber, ...restNumbers) ||
+      evaluate(target, Number(`${acc}${nextNumber}`), ...restNumbers)
+    );
+  };
+
+  return map.reduce((acc, [target, ...numbers]) => {
+    return acc + (evaluate(target, ...numbers) ? target : 0);
+  }, 0);
+};
