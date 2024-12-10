dfc1f20026ee4f0c41c3bc43f24ceb465ea20063
Vitalii Petrychuk <vitaliy@petrychuk.com>
2024-12-10 17:30:00 +0100

    Day 10

---
+++ 2024/day-10/2.js
@@ -0,0 +1,47 @@
+module.exports = (input) => {
+  const grid = input.split("\n").map((row) => row.split("").map(Number));
+  const trailheads = grid
+    .flatMap((row, y) => row.map((val, x) => val === 0 && [y, x]))
+    .filter(Boolean);
+
+  const directions = [
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+  ];
+
+  const getNeighboringHeightPosition = (
+    [currentY, currentX],
+    lookingForHeight
+  ) =>
+    directions
+      .map(([directionY, directionX]) => {
+        const y = currentY + directionY;
+        const x = currentX + directionX;
+        return grid[y]?.[x] === lookingForHeight && [y, x];
+      })
+      .filter(Boolean);
+
+  const scores = trailheads.map((trailhead) => {
+    let stack = [trailhead];
+    let score = 0;
+    let position;
+
+    while ((position = stack.pop())) {
+      const [y, x] = position;
+      if (grid[y][x] === 9) {
+        score += 1;
+      } else {
+        stack = [
+          ...stack,
+          ...getNeighboringHeightPosition(position, grid[y][x] + 1),
+        ];
+      }
+    }
+
+    return score;
+  });
+
+  return scores.reduce((acc, val) => acc + val);
+};
